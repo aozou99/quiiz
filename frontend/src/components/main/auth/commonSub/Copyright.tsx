f6b4b2802685834432801f0c47210bdb18bffe18
aozou99 <hellostacysmom@gmail.com>
2020-07-09 09:59:05 +0900

    フォルダ名修正

---
+++ frontend/src/components/main/auth/commonSub/Copyright.tsx
@@ -0,0 +1,17 @@
+import { Typography, Link } from "@material-ui/core";
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Quiiz
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+export default Copyright;
