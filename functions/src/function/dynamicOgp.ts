2af9fe40c5c2db5916830d00e3194c2e19125a37
aozou99 <hellostacysmom@gmail.com>
2020-10-08 09:12:02 +0900

    動的OGP対応

--- functions/src/function/dynamicOgp.ts
+++ functions/src/function/dynamicOgp.ts
@@ -1,12 +1,62 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import admin from '../core/admin';
+import convertQuizResponse from '../helper/convertQuizResponse';
+
+const db = admin.firestore();
+const prefixPath = '/play/';
 
 module.exports = functions
-  .region("us-central1")
+  .region('us-central1')
   .runWith({
     timeoutSeconds: 30,
-    memory: "2GB",
+    memory: '2GB',
   })
   .https.onRequest(async (req, res) => {
-    console.log(req.path);
-    res.status(200).send(req.path);
+    const quizId = req.path.split(prefixPath)[1];
+    const result = await db
+      .collectionGroup('quizzes')
+      .where('id', '==', quizId)
+      .get();
+    if (result.size < 1) {
+      res.status(404).send('Not Found Quizz');
+      return;
+    }
+    const quiz = await convertQuizResponse(
+      result.docs[0]
+    );
+    const age = 86400 * 30;
+    res.set(
+      'Cache-Control',
+      `public, max-age=${age}, s-maxage=${age}`
+    );
+    res.status(200).send(
+      buildHtml(quizId, {
+        title: quiz.question,
+        text: `${quiz.selectA}, ${quiz.selectB}, ${quiz.selectC}, ${quiz.selectD}`,
+        img: quiz.thumbnail['640x360'],
+      })
+    );
   });
+
+const buildHtml = (id: string, obj: any) => {
+  return `
+  <!DOCTYPE html>
+    <head>
+      <title>${obj.title}</title>
+      <meta property="og:title" content="${obj.title}>
+      <meta property="og:image" content="${obj.img}">
+      <meta
+        property="og:description"
+        content="${obj.text}"
+      />
+      <meta name="twitter:card" content="summary_large_image">
+      <meta name="twitter:title" content="${obj.title}">
+      <meta name="twitter:description" content="${obj.text}">
+      <meta name="twitter:image" content="${obj.img}">
+      <link rel="canonical" href="/play/${id}">
+    </head>
+    <body>
+      <script>window.location="/play/${id}";</script>
+    </body>
+  </html>`;
+};
